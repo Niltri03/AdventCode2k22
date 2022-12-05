8d76fe22468c675b039daf82ba659d96b97b2b2d
Nacho F. - Niltrio <119792167+Niltri03@users.noreply.github.com>
2022-12-05 16:54:15 +0100

    Create Day7Prob1.cc

---
+++ Code2k21/Day7Prob1.cc
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <limits.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a, optimal; 
+    int total = 0; 
+    char c; 
+    int maxval = 0; 
+    optimal = INT_MAX;
+    vector<int> dist; 
+    while(cin >> a >> c){
+        dist.push_back(a);
+        if (a > maxval)maxval = a; 
+    }
+    
+    for(int i = 0; i < maxval; ++i){
+        for(int j = 0 ; j < dist.size(); ++j){
+            if(dist[j] >= i) total += dist[j]-i; 
+            else total += i-dist[j]; 
+        }
+        if (total < optimal){
+            optimal = total; 
+        }
+        total = 0; 
+    }
+    
+    cout << "El valor optimo es " << optimal << endl; 
+    
+}
