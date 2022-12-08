e242636877510ea5333020a3d5241677fd8ca280
Nacho F <119792167+Niltri03@users.noreply.github.com>
2022-12-08 11:46:01 +0100

    Update Day8Prob1.cc

--- Code2k22/Day8Prob1.cc
+++ Code2k22/Day8Prob1.cc
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -15,90 +7,68 @@ using Forest = vector<Row>;
 using Row2 = vector<bool>; 
 using Forest2 = vector<Row2>; 
 
+void writeF2(Forest2 &f){
+    int c = 0; 
+    cout << "-------------" << endl;
+    for(int i = 0; i < f.size(); ++i){
+        for(int j = 0; j < f.size(); ++j){
+            cout << f[i][j];
+            if(f[i][j]) c++; 
+        }
+        cout << endl; 
+    }
+    cout << "----------------" << endl;
+    cout << "En total hay " << c << " árboles visibles." << endl;
+    
+    
+}
+
 int main()
 {
-    //Forest f(99, vector<int>(99));
-    Forest f (5, vector<int>(5)); 
+    Forest f(99, vector<int>(99));
+    //Forest f (5, vector<int>(5)); 
     cout << f.size() << endl; 
-    for(int i = 0; i < 5;++i){
-        for(int j = 0; j < 5; ++j){
+  
+    for(int i = 0; i < 99;++i){
+        for(int j = 0; j < 99; ++j){
             char c; 
             cin >> c;
             f[i][j] = c - '0'; //Lee de izq a derecha y abajo arriba
         }
     }
     Forest2 f2(f.size(), vector<bool>(f.size(), false));
-    int contador = 0; 
-    for(int i = 0; i < f.size();++i){
-    bool b = true; 
-    contador++;
-        for(int j = 0; j < f.size(); ++j){
-            for(int i2 = 0; i2 < i; ++i2){
-                cout << "itero el valor" << i << j << " y lo comparo con " << i2 << j << endl; 
-                if(f[i2][j] > f[i][j]){
-                        b = false; 
-                        i2 = i; 
-                }
-                if(b) f2[i][j] = true; 
-                b = false; 
-            }
-            
-        }
-    }
-    cout << contador << endl; 
-    for(int i = f.size()-1; i >= 0;++i){
-    contador++; 
-    bool b = true; 
-        for(int j = f.size()-1; j >= 0; ++j){
-            for(int i2 = f.size()-1; i2 > i; --i2){
-                if(f[i2][j] > f[i][j]){
-                        b = false; 
-                        i2 = i; 
-                }
-                if(b) f2[i][j] = true; 
-                b = false; 
-            }
-            
-        }
-    }
-    cout << contador << endl; 
-    for(int i = 0; i < f.size();++i){
-    bool b = true; 
-    contador++;
-        for(int j = 0; j < f.size(); ++j){
-            for(int j2 = 0; j2 < j; ++j2){
-                if(f[i][j2] > f[i][j]){
-                        b = false; 
-                        j2 = j; 
-                }
-                if(b) f2[i][j] = true; 
-                b = false; 
-            }
-            
-        }
-    }
-    cout << contador << endl; 
-    for(int i = f.size()-1; i >= 0;++i){
-    contador++; 
-    bool b = true; 
-        for(int j = f.size()-1; j >= 0; ++j){
-            for(int j2 = f.size()-1; j2 > j; ++j2){
-                if(f[i][j2] > f[i][j]){
-                        b = false; 
-                        j2 = j; 
-                }
-                if(b) f2[i][j] = true; 
-                b = false; 
-            }
-            
-        }
-    }
-    cout << contador << endl; 
+
     for(int i = 0; i < f.size(); ++i){
-        for(int j = 0; j < f.size(); ++j){
-            cout << f2[i][j];
-        }
-        cout << endl; 
+        f2[i][0] = true; 
+        f2[i][f.size()-1]  = true; 
     }
+    for(int i = 0; i < f.size(); ++i){
+        f2[0][i] = true; 
+        f2[f.size()-1][i]  = true; 
+    }
+    
+    
+    for(int i = 1; i < f.size()-1; ++i){
+        for(int j = 1; j < f.size()-1; ++j){
+            bool t, b, r, l; //Bools indican si está tapado por algún lado.
+            t = false;
+            b = false; 
+            r = false; 
+            l = false; 
+            for(int i2 = i-1; i2 >= 0; --i2) if(f[i][j] <= f[i2][j]) t=true;
+            for(int i2 = f.size()-1; i2 > i; --i2) if(f[i][j] <= f[i2][j]) b=true;
+            for(int j2 = j-1; j2 >= 0; --j2) if(f[i][j] <= f [i][j2]) l = true; 
+            for(int j2 = f.size()-1; j2 > j; --j2) if(f[i][j] <= f[i][j2]) r=true;
+            if(not t or not b or not r or not l)f2[i][j] = true; 
+        }
+    }
+    
+    
+    
+    
+
+    writeF2(f2); 
+    
+    
     
 }
