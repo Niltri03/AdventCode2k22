f27b4485b9512cfe55ca62b87c4536661b962bca
Nacho F <119792167+Niltri03@users.noreply.github.com>
2022-12-09 12:27:47 +0100

    Update Prob1Tests.cc

--- Code2k22/Day9/Prob1Tests.cc
+++ Code2k22/Day9/Prob1Tests.cc
@@ -21,6 +21,18 @@ void countVisited(mInt &f){
     cout << "Casillas visitadas: " << max << endl;
 }
 
+void writeMatC(mChar &m){
+    cout << "-----------------------" << endl;
+    for(int i = 0; i < m.size(); ++i){
+        for(int j = 0; j < m.size(); ++j){
+            cout << m[i][j];
+        }
+        cout << endl; 
+    }
+    cout << "-----------------------" << endl; 
+    
+}
+
 int main()
 {
     //mInt VP(100, Row(100, 0));
@@ -35,18 +47,19 @@ int main()
     int i;
     while(cin >> dir){
         cin >> i; 
-        if( T == H ) 
         while(i > 0){
-            VP[H.first][H.second] == '.';
+            VP[H.first][H.second] = '.';
             if(dir == 'D') H.first += 1;
             if(dir == 'U') H.first -= 1; 
             if(dir == 'R') H.second += 1; 
             if(dir == 'L') H.second -= 1; 
-            VP[H.first][H.second] == 'H';
+            cout << H.first << " " << H.second << endl; 
+            VP[H.first][H.second] = 'H';
             --i; 
+            writeMatC(VP); 
         }
         
     }
     
-    countVisited(VP); 
+    //countVisited(VP); 
 }
