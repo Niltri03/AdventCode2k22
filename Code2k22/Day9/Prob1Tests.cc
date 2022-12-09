5b7272e6b01e067f5d0096a804af6ccab44551e6
Nacho F <119792167+Niltri03@users.noreply.github.com>
2022-12-09 11:38:17 +0100

    Create Prob1Tests.cc

---
+++ Code2k22/Day9/Prob1Tests.cc
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using Row = vector<int>; 
+using mInt = vector<Row>; 
+using Row2 = vector<char>; 
+using mChar = vector<Row2>; 
+
+
+void countVisited(mInt &f){
+    int max = 0; 
+    cout << "-------------" << endl;
+    for(int i = 0; i < f.size(); ++i){
+        for(int j = 0; j < f.size(); ++j){
+            if (f[i][j] > 0) max++; 
+        }
+    }
+    cout << "Casillas visitadas: " << max << endl;
+}
+
+int main()
+{
+    //mInt VP(100, Row(100, 0));
+    mChar VP(5, Row2(5, '.'));
+    pair<int, int> T; 
+    pair<int, int> H; 
+    T.first = 0; 
+    T.second = 0; 
+    H.first = 0; 
+    H.second = 0; 
+    char dir; 
+    int i;
+    while(cin >> dir){
+        cin >> i; 
+        if( T == H ) 
+        while(i > 0){
+            VP[H.first][H.second] == '.';
+            if(dir == 'D') H.first += 1;
+            if(dir == 'U') H.first -= 1; 
+            if(dir == 'R') H.second += 1; 
+            if(dir == 'L') H.second -= 1; 
+            VP[H.first][H.second] == 'H';
+            --i; 
+        }
+        
+    }
+    
+    countVisited(VP); 
+}
