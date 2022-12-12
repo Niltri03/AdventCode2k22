eb3923b2ef2b028a96bcc940ae8f65c9a8ce9193
Nacho F <119792167+Niltri03@users.noreply.github.com>
2022-12-12 18:36:06 +0100

    Create Day12Prob1.cc

---
+++ Code2k22/Day12Prob1.cc
@@ -0,0 +1,78 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+// z > Z
+
+typedef vector < vector < int >> graph; 
+
+
+void backtracking(graph m, int it, int &curRet, int r, int c){
+        if((m[r][c] == 26) and (it < curRet)){
+            curRet = it;
+             
+        }
+        else if ((it >= curRet) or (m[r][c] == 2147483647));
+        else{
+            //cout << "Funciona en la operacion" << r << "," << c << endl; 
+            if((m[r][c+1] == m[r][c]+1) or (m[r][c+1] == m[r][c])){
+                int aux; 
+                aux = m[r][c];
+                m[r][c] = 2147483647; 
+                backtracking(m,it+1,curRet, r, c+1);
+                m[r][c] = aux; 
+            }
+            if((m[r+1][c] == m[r][c]+1) or (m[r+1][c] == m[r][c])){
+                int aux; 
+                aux = m[r][c];
+                m[r][c] = 2147483647; 
+                backtracking(m,it+1,curRet, r+1, c);
+                m[r][c] = aux; 
+            }
+            if((m[r-1][c] == m[r][c]+1) or (m[r-1][c] == m[r][c])){
+                int aux; 
+                aux = m[r][c];
+                m[r][c] = 2147483647; 
+                backtracking(m,it+1,curRet, r-1, c);
+                m[r][c] = aux; 
+            }
+            
+            if((m[r][c-1] == m[r][c]+1)or (m[r][c-1] == m[r][c])){
+                int aux; 
+                aux = m[r][c];
+                m[r][c] = 2147483647; 
+                backtracking(m,it+1,curRet, r, c-1);
+                m[r][c] = aux; 
+            }
+            cout << "sigo vivo, it: " << it << ", ret: " << curRet << endl;
+        }
+        
+    }
+
+int main()
+{
+    int ret = 300; 
+    int nrow = 41; 
+    int ncol = 173; 
+    graph mapps(nrow+2, vector<int>(ncol+2,200));
+    int x0, y0; 
+    for(int i = 1; i <= nrow; ++i){
+        for(int i2 = 1; i2 <= ncol; ++i2){
+            char c; 
+            cin >> c; 
+            if(c == 'S'){
+                mapps[i][i2] = 0; 
+                x0 = i; 
+                y0 = i2; 
+            }
+            else if (c == 'E'){
+                mapps[i][i2] = 26; 
+            }
+            else mapps[i][i2] = c - 'a';
+        }
+        
+    }
+    cout << "He leído bien" << endl;
+    backtracking(mapps, 0, ret, x0, y0);
+    cout << "Resultado: " << ret << endl; 
+}
