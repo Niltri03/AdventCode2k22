5e150e85372cdd309c28ed7e71d9100d890b0940
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-04-02 17:42:10 +0200

    Create Day01.java

---
+++ Code2k22Java/Day01.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+//For this to work, all empty lines have to be replaced with a dash (-). 
+
+import static java.lang.System.exit;
+
+
+public class Main {
+
+    public static void ejecuta(Scanner sc){
+        Integer suma = 0;
+        Integer topval1 = 0;
+        Integer topval2 = 0;
+        Integer topval3 = 0;
+        while (sc.hasNext()) {
+            String i = sc.next();
+            if (i.equals("-")) {
+                if (suma > topval1) {
+                topval3 = topval2;
+                topval2 = topval1;
+                topval1 = suma;
+            }else if (suma > topval2) {
+                topval3 = topval2;
+                topval2 = suma;
+            }
+
+                 else if (suma > topval3) {
+                    topval3 = suma;
+                }
+
+                suma = 0;
+            }
+            else {
+
+               Integer x = Integer.parseInt(i);
+
+                suma += x;
+                System.out.println(i);
+                System.out.println("Suma: " + suma);
+                System.out.println("Maxval1: " + topval1);
+                System.out.println("Maxval2: " + topval2);
+                System.out.println("Maxval3: " + topval3);
+                Integer sumaTotal = topval1 +topval2+topval3;
+                System.out.println("Suma: " + sumaTotal);
+
+            }
+
+        }
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        try{
+            Scanner sc = new Scanner(new File("C:\\Users\\Nacho\\Documents\\programas\\Java\\helloWorld\\src\\data.txt"));
+            ejecuta(sc);
+        }
+        catch(FileNotFoundException ex)
+        {
+            exit(0);
+        }
+
+
+
+    }
+}
+
