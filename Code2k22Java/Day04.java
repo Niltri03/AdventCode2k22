943cdd7abe61d967bddfca1dee40d011778be479
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-04-03 01:29:26 +0200

    Create Day04.java

---
+++ Code2k22Java/Day04.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+import static java.lang.System.exit;
+
+
+public class Main {
+
+    public static void ejecuta(Scanner sc) {
+        Integer sumatorioOverlapsTotales = 0;
+        Integer sumatorioOverlapsParciales = 0;
+
+        while (sc.hasNext()) {
+            
+            //Works if the input is an int / line. 
+            Integer x1 = sc.nextInt();
+            Integer x2 = sc.nextInt();
+            Integer y1 = sc.nextInt();
+            Integer y2 = sc.nextInt();
+
+
+
+
+
+
+           if ((y1 >= x1 && y2 <= x2) || (x1 >= y1 && x2 <= y2)){
+                sumatorioOverlapsTotales++;
+            }
+            if(!(x1 >y2 || y1 > x2)){
+                sumatorioOverlapsParciales++;
+            }
+        }
+        System.out.println("Suma overlaps totales: " + sumatorioOverlapsTotales);
+        System.out.println("Suma overlaps parciales: " + sumatorioOverlapsParciales);
+
+
+        }
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        try{
+            Scanner sc = new Scanner(new File("C:\\Users\\Nacho\\Documents\\programas\\Java\\helloWorld\\src\\data.txt"));
+            ejecuta(sc);
+        }
+        catch(FileNotFoundException ex)
+        {
+            exit(0);
+        }
+
+
+
+    }
+}
+
