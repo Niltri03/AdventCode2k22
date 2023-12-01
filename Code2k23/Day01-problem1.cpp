ed648a590ed5ce63b3749da90b9bccaa993167ad
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-01 06:52:06 +0100

    Create Day01-problem1.cpp

---
+++ Code2k23/Day01-problem1.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std; 
+
+int main(){
+    string st;
+    int firstDigit, lastDigit, aux, result;
+    aux = result = 0; 
+    while(cin >> st and st != "done"){
+        for(int i = 0; i < st.length(); i++){
+            if(st[i] > '0' and st[i] <= '9'){
+                int a = st[i] - '0';
+                if(firstDigit == 0) firstDigit = a; 
+                else lastDigit = a; 
+            }
+        }
+        if(lastDigit == 0) lastDigit = firstDigit; 
+        aux += lastDigit; 
+        aux += 10 * firstDigit;
+        result += aux;  
+        aux = 0; 
+        lastDigit = 0; 
+        firstDigit = 0; 
+    }
+    cout << result;
+}
