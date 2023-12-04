8d0ecf05fe35b31c7b745b0a6c0482f14a8cf76e
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-04 11:51:11 +0100

    Update Day01.cc

--- Code2k15/Day01Problem01.cc
+++ Code2k15/Day01Problem01.cc
@@ -4,15 +4,20 @@ using namespace std;
 
 int main(){
     //string s = /*your input*/ 
-    int floor = 0; //counter of floors
-    for(int i = 0; i < s.length(); ++i){//for each button press 
+    int floor = 0; //result
+    bool finding = true; //for problem 2
+    for(int i = 0; i < s.length(); ++i){//for each char in our string
         
-            if(s[i] == '(') floor++; //check if we go up 
-            else floor--; //else we go down 
-            //cout << i << endl; 
-            cout << "Floor: " << floor << endl;//write the result 
+            if(s[i] == '(') floor++; 
+            else floor--; 
+            if(floor < 0 and finding){//aka the first time we enter the basement
+                cout << i++ << endl;//solution to problem 2
+                finding = !finding;//we aren't finding anymore
+            }
+            
         
     }
+    cout << "Floor: " << floor; //solution to problem 1
       
 
 }
