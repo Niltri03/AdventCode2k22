0b2707e3d203100bf005746d9ee73bfd42d77a9c
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-04 11:46:24 +0100

    Create Day01Problem01.cc

---
+++ Code2k15/Day01Problem01.cc
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+using namespace std; 
+
+int main(){
+    //string s = /*your input*/ 
+    int floor = 0; //counter of floors
+    for(int i = 0; i < s.length(); ++i){//for each button press 
+        
+            if(s[i] == '(') floor++; //check if we go up 
+            else floor--; //else we go down 
+            //cout << i << endl; 
+            cout << "Floor: " << floor << endl;//write the result 
+        
+    }
+      
+
+}
