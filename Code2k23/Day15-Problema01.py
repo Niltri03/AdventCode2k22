8d4275d74e01786cecd8edb8f1c77f2674806ae3
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-15 13:07:45 +0100

    Update Day15-Problema01.py

--- Code2k23/Day15-Problema01.py
+++ Code2k23/Day15-Problema01.py
@@ -1,5 +1,16 @@
 f = open("input15.txt")
 s = f.read()
-print(s)
-inp = s.split(",")
-#print(inp)
+cosas = s.split(",")
+print(cosas)
+retFinal = 0
+for palabra in cosas:
+    ret = 0
+    for letra in palabra:
+        a = 0;
+        a = a+ord(letra)
+        a = a * 17
+        a = a % 256
+        ret = ret + a
+    retFinal = retFinal + ret
+    print ret
+print retFinal
